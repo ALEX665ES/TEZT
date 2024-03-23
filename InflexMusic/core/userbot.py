e94ae010aa02e5731dcec4c650122a8449a4eb8b
Ａｌｅｘ <127571445+ALEX665ES@users.noreply.github.com>
2024-03-23 09:04:31 +0530

    Update userbot.py

--- InflexMusic/core/userbot.py
+++ InflexMusic/core/userbot.py
@@ -51,11 +51,11 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("InflexUpdates")
-                await self.one.join_chat("InflexSupport")
-                await self.one.join_chat("TeamInflex")
-                await self.one.join_chat("InflexStatus")
-                await self.one.join_chat("InflexBots")
+                await self.one.join_chat("CiNE_PLiX")
+                await self.one.join_chat("AbOuTInNoCeNt")
+                await self.one.join_chat("SCIENCE_CLUBS")
+                await self.one.join_chat("AryanStudyGroup")
+                await self.one.join_chat("Science_92")
             except:
                 pass
             assistants.append(1)
@@ -75,8 +75,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.one.join_chat("TeamInflex")
-                await self.one.join_chat("InflexSupport")
+                await self.one.join_chat("CiNE_PLiX")
+                await self.one.join_chat("AbOuTInNoCeNt")
             except:
                 pass
             assistants.append(2)
@@ -96,11 +96,11 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.one.join_chat("InflexUpdates")
-                await self.one.join_chat("InflexSupport")
-                await self.one.join_chat("TeamInflex")
-                await self.one.join_chat("InflexStatus")
-                await self.one.join_chat("InflexBots")
+                await self.one.join_chat("CiNE_PLiX")
+                await self.one.join_chat("AbOuTInNoCeNt")
+                await self.one.join_chat("SCIENCE_CLUBS")
+                await self.one.join_chat("AryanStudyGroup")
+                await self.one.join_chat("Science_92")
             except:
                 pass
             assistants.append(3)
@@ -120,11 +120,11 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.one.join_chat("InflexUpdates")
-                await self.one.join_chat("InflexSupport")
-                await self.one.join_chat("TeamInflex")
-                await self.one.join_chat("InflexStatus")
-                await self.one.join_chat("InflexBots")
+                await self.one.join_chat("CiNE_PLiX")
+                await self.one.join_chat("AbOuTInNoCeNt")
+                await self.one.join_chat("SCIENCE_CLUBS")
+                await self.one.join_chat("AryanStudyGroup")
+                await self.one.join_chat("Science_92")
             except:
                 pass
             assistants.append(4)
@@ -144,11 +144,11 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.one.join_chat("InflexUpdates")
-                await self.one.join_chat("InflexSupport")
-                await self.one.join_chat("TeamInflex")
-                await self.one.join_chat("InflexStatus")
-                await self.one.join_chat("InflexBots")
+                await self.one.join_chat("CiNE_PLiX")
+                await self.one.join_chat("AbOuTInNoCeNt")
+                await self.one.join_chat("SCIENCE_CLUBS")
+                await self.one.join_chat("AryanStudyGroup")
+                await self.one.join_chat("Science_92")
             except:
                 pass
             assistants.append(5)
@@ -180,3 +180,4 @@ class Userbot(Client):
                 await self.five.stop()
         except:
             pass
+      
